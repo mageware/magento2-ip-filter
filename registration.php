e50bc016ee5c90e1894b5347b6de3e39666a2d90
Slawomir Iwanczuk <iwanczuk@users.noreply.github.com>
2017-01-17 13:33:53 +0100

    Created module structure

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * See LICENSE.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageWare_IpFilter',
+    __DIR__
+);
